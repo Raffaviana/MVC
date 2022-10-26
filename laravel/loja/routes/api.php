35e0409cc0ad27dcde2d29c3f107e702d4fa5468
JACKSON RAFAEL DE JESUS VIANA <jackson.rjviana@senacsp.edu.br>
2022-10-25 22:00:17 -0300

    aula-25-10-22

--- laravel/loja/routes/api.php
+++ laravel/loja/routes/api.php
@@ -17,3 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(static function(){
+    Route::get('/vendedores', [App\Http\Controllers\vendedorapicontroller::class, 'index']);
+    Route::post('/vendedores', [App\Http\Controllers\vendedorapicontroller::class, 'store']);
+    Route::delete('/vendedores/{id}', [App\Http\Controllers\vendedorapicontroller::class, 'destroy']);
+    Route::get('/vendedores/{id}', [App\Http\Controllers\vendedorapicontroller::class, 'show']);
+    Route::put('/vendedores/{id}', [App\Http\Controllers\vendedorapicontroller::class, 'update']);
+
+    Route::get('/clientes', [App\Http\Controllers\clienteapicontroller::class, 'index']);
+    Route::post('/clientes', [App\Http\Controllers\clienteapicontroller::class, 'store']);
+    Route::delete('/clientes/{id}', [App\Http\Controllers\clienteapicontroller::class, 'destroy']);
+    Route::get('/clientes/{id}', [App\Http\Controllers\clienteapicontroller::class, 'show']);
+    Route::put('/clientes/{id}', [App\Http\Controllers\clienteapicontroller::class, 'update']);
+
+    Route::get('/produtos', [App\Http\Controllers\produtoapicontroller::class, 'index']);
+    Route::post('/produtos', [App\Http\Controllers\produtoapicontroller::class, 'store']);
+    Route::delete('/produtos/{id}', [App\Http\Controllers\produtoapicontroller::class, 'destroy']);
+    Route::get('/produtos/{id}', [App\Http\Controllers\produtoapicontroller::class, 'show']);
+    Route::put('/produtos/{id}', [App\Http\Controllers\produtoapicontroller::class, 'update']);
+});
